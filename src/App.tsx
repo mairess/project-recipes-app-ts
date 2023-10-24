0f630efdaaa695ef6eb0a2c1fa2495fc8ddfd715
AmsBarros <amandabarros02@msn.com>
2023-10-24 19:42:26 -0300

    Refatorando tela de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
-import { AppProvider } from './context/AppContext';
 
 function App() {
   return (
-    <AppProvider>
-      <Routes>
-        <Route path="/" element={ <Login /> } />
-      </Routes>
-    </AppProvider>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+    </Routes>
   );
 }
 
