61de2a616fad450fa27cac521b7f281b642800ed
AmsBarros <amandabarros02@msn.com>
2023-10-25 20:57:08 -0300

    Continuação dos testes

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -6,6 +6,9 @@ import renderWithRouter from './helpers/renderWith';
 /* TESTES PARA O SEARCH BAR */
 
 const searchTitleTestId = 'search-top-btn';
+const searchBtnTestId = 'exec-search-btn';
+const ingredientBtnTestId = 'ingredient-search-radio';
+const searchInputTestId = 'search-input';
 
 test('Verifica se o botão de search leva pra rota certa', async () => {
   const { user } = renderWithRouter(<App />, { route: '/meals' });
@@ -15,55 +18,94 @@ test('Verifica se o botão de search leva pra rota certa', async () => {
   await waitFor(() => expect(window.location.pathname).toBe('/meals'));
 });
 
-test('Verifica se o fetch funciona corretamente', async () => {
+test('Verifica se o fetch de meals funciona corretamente', async () => {
   const { user } = renderWithRouter(<App />, { route: '/meals' });
-  const mockData = {
-    strMeal: 'Brown Stew Chicken',
-    strMealThumb: 'https://www.themealdb.com/images/media/meals/sypxpx1515365095.jpg',
-    idMeal: '52940',
-  };
-  const MOCK_RESPONSE = {
-    json: async () => mockData,
-  } as Response;
-  vi.spyOn(global, 'fetch').mockResolvedValue(MOCK_RESPONSE);
+  const alert = vi.spyOn(window, 'alert');
+
+  vi.spyOn(global, 'fetch');
 
   const iconSearchBtn = screen.getByTestId(searchTitleTestId);
 
   await user.click(iconSearchBtn);
 
-  const botaoSearch = screen.getByTestId('exec-search-btn');
-  const testaBotaoRadio = screen.getByTestId('ingredient-search-radio');
-  const inputType = screen.getByTestId('search-input');
+  const botaoSearch = screen.getByTestId(searchBtnTestId);
+  const testaBotaoRadio = screen.getByTestId(ingredientBtnTestId);
+  const inputType = screen.getByTestId(searchInputTestId);
 
   await user.type(inputType, 'tomato');
   await user.click(testaBotaoRadio);
   await user.click(botaoSearch);
+
+  expect(fetch).toHaveBeenCalled();
+  const mealList = await screen.findAllByTestId(/card-img/i);
+  expect(mealList).toHaveLength(9);
+
+  user.clear(inputType);
+
+  await user.type(inputType, 'brown');
+  const radioBtnName = screen.getByTestId('name-search-radio');
+  await user.click(radioBtnName);
+  await user.click(botaoSearch);
+
+  user.clear(inputType);
+
+  await user.type(inputType, 'a');
+  const radioBtnFL = screen.getByTestId('first-letter-search-radio');
+  await user.click(radioBtnFL);
+  await user.click(botaoSearch);
+
+  const profileBtn = screen.getByTestId('profile-top-btn');
+
+  user.clear(inputType);
+
+  await user.type(inputType, 'coisa-que-nao-exite');
+  await user.click(botaoSearch);
+
+  expect(alert).toBeCalledWith('Your search must have only 1 (one) character');
+  await user.click(profileBtn);
+
+  await waitFor(() => expect(window.location.pathname).toBe('/profile'));
 });
 
-test('Verifica se o fetch funciona corretamente', async () => {
+test('Verifica se o fetch drinks funciona corretamente', async () => {
   const { user } = renderWithRouter(<App />, { route: '/drinks' });
 
-  const mockData = {
-    strDrink: 'Mango Orange Smoothie',
-    strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/vdp2do1487603520.jpg',
-    idDrink: '12716',
-  };
-
-  const MOCK_RESPONSE = {
-    json: async () => mockData,
-  } as Response;
-
-  vi.spyOn(global, 'fetch').mockResolvedValue(MOCK_RESPONSE);
+  vi.spyOn(global, 'fetch');
 
   const iconSearchBtn = screen.getByTestId(searchTitleTestId);
 
   await user.click(iconSearchBtn);
 
-  const botaoSearch = screen.getByTestId('exec-search-btn');
-  const testaBotaoRadio = screen.getByTestId('ingredient-search-radio');
-  const inputType = screen.getByTestId('search-input');
+  const botaoSearch = screen.getByTestId(searchBtnTestId);
+  const testaBotaoRadio = screen.getByTestId(ingredientBtnTestId);
+  const inputType = screen.getByTestId(searchInputTestId);
 
-  await user.type(inputType, 'Roberto');
+  await user.type(inputType, 'orange');
   await user.click(testaBotaoRadio);
   await user.click(botaoSearch);
+
+  expect(fetch).toHaveBeenCalled();
+  const mealList = await screen.findAllByTestId(/card-img/i);
+  expect(mealList).toHaveLength(4);
+});
+
+test('Verifica se o alert é chamado', async () => {
+  const { user } = renderWithRouter(<App />, { route: '/meals' });
+  const alert = vi.spyOn(window, 'alert');
+
+  vi.spyOn(global, 'fetch');
+
+  const iconSearchBtn = screen.getByTestId(searchTitleTestId);
+
+  await user.click(iconSearchBtn);
+
+  const botaoSearch = screen.getByTestId(searchBtnTestId);
+  const testaBotaoRadio = screen.getByTestId(ingredientBtnTestId);
+  const inputType = screen.getByTestId(searchInputTestId);
+
+  await user.type(inputType, 'outra coisa');
+  await user.click(testaBotaoRadio);
+  await user.click(botaoSearch);
+
+  waitFor(() => expect(alert).toBeCalledWith("Sorry, we haven't found any recipes for these filters."));
 });
