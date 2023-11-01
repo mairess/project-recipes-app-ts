fbff77bb6d6476d507a35cbed241d751536981e5
mairess <trabalho.maires@gmail.com>
2023-11-01 18:59:00 -0300

    feat: implement FilterBar

---
+++ src/components/FilterBar.tsx
@@ -0,0 +1,43 @@
+// import ButtonFavoriteRecipes from './ButtonFavoriteRecipes';
+// import iconAllFoods from '../images/iconAllFoods.svg';
+// import incoMeals from '../images/iconMeals.svg';
+// import incoDrinks from '../images/iconDrinks.svg';
+// import iconFavorite from '../images/iconFavorite.svg';
+// import { Title, Contaiber } from './styles/FilterBarStyle';
+
+// function FilterBar() {
+//   return (
+//     <>
+//       <Title>
+//         <img src={ iconFavorite } alt="Favorite" />
+//         <h1
+//           data-testid="page-title"
+//         >
+//           Favorite Recipes
+//         </h1>
+//       </Title>
+//       <Contaiber>
+//         <ButtonFavoriteRecipes
+//           datatestid="filter-by-all-btn"
+//           imgSrc={ iconAllFoods }
+//           altText="AllFoods"
+//           buttonText="All foods"
+//         />
+//         <ButtonFavoriteRecipes
+//           datatestid="filter-by-meal-btn"
+//           imgSrc={ incoMeals }
+//           altText="Only foods"
+//           buttonText="Meals"
+//         />
+//         <ButtonFavoriteRecipes
+//           datatestid="filter-by-drink-btn"
+//           imgSrc={ incoDrinks }
+//           altText="Only foods"
+//           buttonText="Drinks"
+//         />
+//       </Contaiber>
+//     </>
+//   );
+// }
+
+// export default FilterBar;
---
+++ src/components/styles/FilterBarStyle.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Contaiber = styled.div`
+display: flex;
+justify-content: space-around;
+align-self: center;
+`;
+
+export const Title = styled.div`
+margin-top: 1.25rem;
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+
+h1 {
+margin-top: 1.25rem;
+color: #41197F;
+text-align: center;
+font-size: 20px;
+font-style: normal;
+font-weight: 900;
+line-height: normal;
+letter-spacing: 2.1px;
+text-transform: uppercase;
+}
+`;
